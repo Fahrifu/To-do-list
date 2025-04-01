46ea723c13e3014629defbd935cc16c64e9eaaf4
Fahrifu <82388717+Fahrifu@users.noreply.github.com>
2025-04-01 14:21:40 +0200

    Task logic

--- app.mjs
+++ app.mjs
@@ -1,5 +1,25 @@
 // DOM elements
+const taskInput = document.getElementById('taskInput');
+const addBtn = document.getElementById('addBtn');
+const taskList = document.getElementById('TaskList'); 
 
 // Task Handler
+addBtn.addEventListener('click', () => {
+    const taskText = taskInput.value.trim();
+    if (taskText = '') return;
 
-// Task Element
+    // Task Element
+    const taskDiv = document.createElement('div');
+    taskDiv.classList.add('task');
+
+    taskDiv.innerHTML = `
+     <input type="checkbox" class="completeCheck" />
+     <span class="taskText">${taskText}</span>
+     <button class"editBtn">Edit</button>
+     <button class"deleteBtn">Delete</button>
+    `;
+
+    taskList.appendChild(taskDiv)
+    taskInput.value = ' ';
+})
+
